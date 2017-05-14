16eb3480058ef6768f59a972bd93ac396527fba0
Jacob Packert <jacob.packert@mediacom.dk>
2017-05-14 12:27:18 +0200

    adding setMessage function

--- src/assets/main.js
+++ src/assets/main.js
@@ -5,6 +5,9 @@ let attempt = document.getElementById('attempt');
 function guess() {
     let input = document.getElementById('user-guess');
     //add functionality to guess function here
+    if(answer === '' || attempt === ''){
+      setHiddenFields();
+    }
 }
 
 //implement new functions here
@@ -15,4 +18,8 @@ function setHiddenFields(){
     answer.value = "0" + answer.value;
   }
   attempt = 0;
+}
+
+function setMessage(message){
+  document.getElementById('message').innerHTML = message;
 }
